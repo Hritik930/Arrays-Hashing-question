387f4397fce97e99d876665ed348938c91bb3e28
Sahil Kasaudhan <157963657+Hritik930@users.noreply.github.com>
2025-07-10 04:40:49 +0530

    Update MoveZeroes.java

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -3,11 +3,13 @@ public class Solution{
     int pos = 0;
     for(int num : nums){
       if(num != 0){
-        nums[pos++] = num;
+        nums[pos] = num;
+        pos++;
       }
     }
     while(n < nums.length){
-      nums[pos++] = 0;
+      nums[pos] = 0;
+      pos++;
     }
   }
 }
